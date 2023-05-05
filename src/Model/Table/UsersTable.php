0d194b9c95bba05d90bc646ad5934217de1abc26
Anurag <anurag3386@gmail.com>
2023-05-05 23:33:17 +0530

    all files including vendor

--- src/Model/Table/UsersTable.php
+++ src/Model/Table/UsersTable.php
@@ -11,6 +11,7 @@
           return $validator
               ->notEmptyString('email', 'An email is required')
               ->email('email')
+              ->add('email', 'unique', ['rule' => 'validateUnique', 'provider' => 'table','message' => 'Seems email already registed. Please use different one.'])
               ->notEmptyString('password', 'A password is required')
               ->notEmptyString('role', 'A role is required')
               ->add('role', 'inList', [
